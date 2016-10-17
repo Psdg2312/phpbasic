d6a1372b374505c840a2bfd32034093dc3d43633
Psdg2312 <mananya.rueajit@gmail.com>
2016-10-17 14:46:11 +0700

    first commit

---
+++ if1.php
@@ -0,0 +1,19 @@
+<?php
+
+$cat='red';
+
+if($cat=='red'){
+    echo 'cat is red';
+    
+} else {
+    echo 'cat is not red';
+    
+    
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
